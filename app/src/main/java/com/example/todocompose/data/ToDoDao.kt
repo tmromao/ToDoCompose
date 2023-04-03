96c70ba9d9f37b99e2c69ff5d9e9468bc6be84b5
tromao <tiagomromao@gmail.com>
2023-04-03 15:05:31 +0100

    Setup Room database

---
+++ app/src/main/java/com/example/todocompose/data/ToDoDao.kt
@@ -0,0 +1,41 @@
+package com.example.todocompose.data
+
+import androidx.room.*
+import com.example.todocompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table WHERE id = :taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTask(toDoTask: ToDoTask)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE '%' || :searchQuery || '%' OR " +
+            "description LIKE '%' || :searchQuery || '%'")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority = 'HIGH' THEN 1 " +
+            "WHEN priority = 'MEDIUM' THEN 2 WHEN priority = 'LOW' THEN 3 " +
+            "ELSE 4 END")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority = 'HIGH' THEN 1 " +
+            "WHEN priority = 'MEDIUM' THEN 2 WHEN priority = 'LOW' THEN 3 " +
+            "ELSE 4 END")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+}
